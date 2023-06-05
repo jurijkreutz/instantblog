b5f3eb640fa950161b73e4e7c0ce49fd3886fecd
jurijkreutz <jurij.kreutz97@gmail.com>
2023-06-05 12:13:25 +0200

    init: initialize backend & frontend

---
+++ backend/src/main/java/com/kreutz/instantblog/InstantblogApplication.java
@@ -0,0 +1,13 @@
+package com.kreutz.instantblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstantblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstantblogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kreutz/instantblog/InstantblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kreutz.instantblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
